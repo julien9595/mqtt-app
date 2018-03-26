59720123dc1c64e78960b890493c45f125f4724a
erwanlbp <erwan.lbp@gmail.com>
2018-03-26 11:55:00 +0200

    Ajoute l'exemple de la lib mqtt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MqttMessage, MqttService} from 'ngx-mqtt';
+import {Observable} from 'rxjs/Observable';
+
+export const MQTT_SERVICE_OPTIONS = {
+  hostname: 'm23.cloudmqtt.com',
+  port: 14962,
+  path: '/'
+};
+
+export function mqttServiceFactory() {
+  return new MqttService(MQTT_SERVICE_OPTIONS);
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  public myOtherMessage$: Observable<MqttMessage>;
+  public myMessage: string;
+
+  constructor(private _mqttService: MqttService) {
+  }
+
+  ngOnInit() {
+    this._mqttService.observe('my/topic').subscribe((message: MqttMessage) => {
+      this.myMessage = message.payload.toString();
+    });
+    this.myOtherMessage$ = this._mqttService.observe('my/other/topic');
+  }
+
+  public unsafePublish(topic: string, message: string): void {
+    this._mqttService.unsafePublish(topic, message, {qos: 1, retain: true});
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
 
-import { AppComponent } from './app.component';
+import {AppComponent, mqttServiceFactory} from './app.component';
+import {MqttModule, MqttService} from 'ngx-mqtt';
 
 
 @NgModule({
@@ -10,9 +11,14 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MqttModule.forRoot({
+      provide: MqttService,
+      useFactory: mqttServiceFactory
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
